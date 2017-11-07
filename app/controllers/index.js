30af181922f56bfbb789b736a7a7ecc088c5e438
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-06 21:46:31 -0800

    add layout, add processResults logic

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -41,31 +41,56 @@ $.searchBar.addEventListener('return', function(e) {
 function processResults(results) {
 	if (results) {
 
+		var sections = [];
+
 		var dataArray = _.map(results.data, function(item) {
 				return {
-						profile: {
-							first_name: item.profile.first_name,
-							middle_name: item.profile.middle_name,
-							last_name: item.profile.last_name,
-							title: item.profile.title,
-							image_url: item.profile.image_url,
-							gender: item.profile.gender,
-							bio: item.profile.bio
-						},
-						location: {
-							city: item.practices[0].visit_address.city,
-							state: item.practices[0].visit_address.state,
-							street: item.practices[0].visit_address.street,
-							zip: item.practices[0].visit_address.zip,
-							lat: item.practices.lat,
-							lon: item.practices.lon,
-							name: item.practices.name,
-							website: item.practices.website,
-						}
+					firstName: item.profile.first_name,
+					middleName: item.profile.middle_name,
+					lastName: item.profile.last_name,
+					title: item.profile.title,
+					imageUrl: item.profile.image_url,
+					gender: item.profile.gender,
+					bio: item.profile.bio,
+					city: item.practices[0].visit_address.city,
+					state: item.practices[0].visit_address.state,
+					street: item.practices[0].visit_address.street,
+					zip: item.practices[0].visit_address.zip,
+					lat: item.practices.lat,
+					lon: item.practices.lon,
+					name: item.practices.name,
+					website: item.practices.website
 				};
 		});
 
+		//if result was 0, exit
+		if (dataArray.length < 1) return;
+
 		console.log(dataArray);
+
+		var sectionHeader = Ti.UI.createView({
+			backgroundColor: "#ececec",
+			width: Ti.UI.FILL,
+			height: 30
+		});
+
+		var sectionLabel = Ti.UI.createLabel({
+			text: "Hello",
+			left: 20,
+			font: {
+				fontSize: 20
+			},
+			color: "#666"
+		});
+		sectionHeader.add(sectionLabel);
+
+		var section = Ti.UI.createListSection({
+		 headerView: sectionHeader
+		});
+
+		$.listView.section = section;
+
+
 	}
 }
 
